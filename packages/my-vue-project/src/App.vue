ea0d4e8788fcc1d13a3a898292df51aa76617819
weiaodi <1635654853@qq.com>
2025-04-22 11:50:31 +0800

    feat(custom):

---
+++ packages/my-vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="msg">{{ msg }}</h1>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const msg = ref('hello word');
+</script>
+
+<style scoped>
+.msg {
+  color: red;
+  font-weight: bold;
+}
+</style>
